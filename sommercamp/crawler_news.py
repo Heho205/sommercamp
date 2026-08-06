92f7894f73a7c8a5b692bfd152910263db9a3b67
Heho205 <helene-205@gmx.de>
2026-08-06 10:36:29 +0000

    crawler news gestertet

---
+++ sommercamp/crawler_news.py
@@ -0,0 +1,70 @@
+#Hier importieren wir die benötigten Softwarebibliotheken.
+from resiliparse.extract.html2text import extract_plain_text
+from scrapy import Spider, Request
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.http.response.html import HtmlResponse
+
+
+class SportSpider(Spider):
+    # Gib hier dem Crawler einen eindeutigen Name,
+    # der beschreibt, was du crawlst.
+    name = "sport"
+
+    start_urls = [
+        # Gib hier mindestens eine (oder mehrere) URLs an,
+        # bei denen der Crawler anfangen soll,
+        # Seiten zu downloaden.
+        "https://www.sportler-des-jahres.de/",
+        "https://www.radsport-rennrad.de/race/radsportler-des-jahres-2025/",
+        "https://www.rennrad-news.de/news/",
+        "https://www.radsport-news.com/",
+        "https://www.eurosport.de/radsport/",
+        "https://www.sportschau.de/radsport"
+
+    ]
+    link_extractor = LxmlLinkExtractor(
+        # Beschränke den Crawler, nur Links zu verfolgen,
+        # die auf eine der gelisteten Domains verweisen.
+        allow_domains=["www.sportler-des-jahres.de", "www.radsport-rennrad.de", "www.rennrad-news.de", "www.radsport-news.com", "www.eurosport.de", "www.sportschau.de"],
+    )
+    custom_settings = {
+        # Identifiziere den Crawler gegenüber den gecrawlten Seiten.
+        "USER_AGENT": "Sommercamp (https://uni-jena.de)",
+        # Der Crawler soll nur Seiten crawlen, die das auch erlauben.
+        "ROBOTSTXT_OBEY": True,
+        # Frage zu jeder Zeit höchstens 4 Webseiten gleichzeitig an.
+        "CONCURRENT_REQUESTS": 4,
+        # Verlangsame den Crawler, wenn Webseiten angeben,
+        # dass sie zu oft angefragt werden.
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 1,
+        # Frage nicht zwei mal die selbe Seite an.
+        "HTTPCACHE_ENABLED": True,
+    }
+
+    def parse(self, response):
+        if not isinstance(response, HtmlResponse):
+            # Die Webseite ist keine HTML-Webseite, enthält also keinen Text.
+            return
+        
+        # Speichere die Webseite als ein Dokument in unserer Dokumentensammlung.
+        yield {
+            # Eine eindeutige Identifikations-Nummer für das Dokument.
+            "docno": str(hash(response.url)),
+            # Die URL der Webseite.
+            "url": response.url,
+            # Der Titel der Webseite aus dem <title> Tag im HTML-Code.
+            "title": response.css("title::text").get(),
+            # Der Text der Webseite.
+            # Um den Hauptinhalt zu extrahieren, benutzen wir
+            # eine externe Bibliothek.
+            "text": extract_plain_text(response.text, main_content=True),
+        }
+
+        # Finde alle Links auf der aktuell betrachteten Webseite.
+        for link in self.link_extractor.extract_links(response):
+            if link.text == "":
+                # Ignoriere Links ohne Linktext, z.B. bei Bildern.
+                continue
+            # Für jeden gefundenen Link, stelle eine Anfrage zum Crawling.
+            yield Request(link.url, callback=self.parse)
